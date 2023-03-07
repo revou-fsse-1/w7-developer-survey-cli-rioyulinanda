ab2ec7ce7a28df14ad20dbe388c650423571e2f8
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-03-07 14:51:25 +0800

    adding survey

--- README.md
+++ README.md
@@ -16,27 +16,32 @@ You can use starter code from folder starter
 
 - What's your first name?
   Rules
+
   - Input type "input"
   - Validate not empty
 
 - Hello (name from question 1) What's your email address?
   Rules
+
   - Input type "input"
   - Validate not empty
 
 - Are you experienced Developer?
   Rules
+
   - Input type "list", [yes, no]
   - Validate not empty
 
 - How many years of experience you have with JavaScript?
   Rules
+
   - Input type "list", ["0-1", "1-3", "3-5", "5-10", "10+"]
   - Validate not empty
   - Only show if question 3 selected "yes"
 
 - What JavaScript library do you know?
   Rules
+
   - Input type "checkbox", ["React.js", "Vue", "Angular", "Node.js", "jQuery", "D3.js"]
   - Validate not empty
   - Only show if question 3 selected "yes"
--- starter/survey.js
+++ starter/survey.js
@@ -1,12 +1,63 @@
 import inquirer from "inquirer";
 
 const questions = [
-  // Type your question here
+  {
+    questions: "What's your first name?",
+  },
+  {
+    questions: "Hello Rio what's your email address?",
+  },
+  {
+    questions: "Are you experienced developer?",
+  },
+  {
+    questions: "How many years of experience you have with JavaScript?",
+  },
+  {
+    questions: "What's JavaScript library do you know?",
+  },
+  {
+    questions: "What is your desired salary?",
+  },
 ];
 
 // run your command
 inquirer
-  .prompt(questions)
+  .prompt([
+    {
+      name: "firstName",
+      message: "What's your first name?",
+      type: "input",
+    },
+    {
+      name: "emailAddress",
+      message: "Hello $(firstName) what's your email address?",
+      type: "input",
+    },
+    {
+      name: "experienceDeveloper",
+      message: "Are you experienced developer?",
+      type: "list",
+      choices: ["yes", "no"],
+    },
+    {
+      name: "javascriptExperience",
+      message: "How many years of experience you have with JavaScript?",
+      type: "list",
+      choices: ["0-1", "1-3", "3-5", "5-10", "10+"],
+    },
+    {
+      name: "javascriptLibrary",
+      message: "What JavaScript library do you know?",
+      type: "checkbox",
+      choices: ["React.js", "Vue", "Angular", "Node.js", "jQuery", "D3.js"],
+    },
+    {
+      name: "desiredSalary",
+      message: "What is your desired salary?",
+      type: "input",
+    },
+  ])
   .then((answers) => {
     console.log(JSON.stringify(answers, null, 2));
   })
