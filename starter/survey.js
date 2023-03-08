b221fe5b092c6cc0a784417de37dd4b941809dbf
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-03-08 19:53:54 +0800

    fixing desired salary question

--- starter/survey.js
+++ starter/survey.js
@@ -2,62 +2,71 @@ import inquirer from "inquirer";
 
 const questions = [
   {
-    questions: "What's your first name?",
+    name: "firstName",
+    message: "What's your first name?",
+    type: "input",
+    validate(answer) {
+      if (!answer) {
+        return "Please, fill your name!";
+      }
+      return true;
+    },
   },
   {
-    questions: "Hello Rio what's your email address?",
+    name: "email",
+    type: "input",
+    message(answers) {
+      return `Hello ${answers.firstName} What's your email address?`;
+    },
+    validate: (answer) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(answer)) {
+        return "You have to provide a valid email address!";
+      }
+      return true;
+    },
   },
   {
-    questions: "Are you experienced developer?",
+    name: "experienceDeveloper",
+    message: "Are you experienced developer?",
+    type: "list",
+    choices: ["yes", "no"],
   },
   {
-    questions: "How many years of experience you have with JavaScript?",
+    name: "javascriptExperience",
+    message: "How many years of experience you have with JavaScript?",
+    type: "list",
+    choices: ["0-1", "1-3", "3-5", "5-10", "10+"],
+    when(answers) {
+      return answers.experienceDeveloper === "yes";
+    },
   },
   {
-    questions: "What's JavaScript library do you know?",
+    name: "javascriptLibrary",
+    message: "What JavaScript library do you know?",
+    type: "checkbox",
+    choices: ["React.js", "Vue", "Angular", "Node.js", "jQuery", "D3.js"],
   },
   {
-    questions: "What is your desired salary?",
+    name: "desiredSalary",
+    message: "What is your desired salary?",
+    type: "input",
+    validate(answer) {
+      if (answer > 10000000) {
+        return "Sorry that's out of our budget!";
+      }
+      if (answer < 10000000) {
+        return "please fill the right amount!";
+      } else {
+        return true;
+      }
+    },
   },
 ];
 
 // run your command
 inquirer
-  .prompt([
-    {
-      name: "firstName",
-      message: "What's your first name?",
-      type: "input",
-    },
-    {
-      name: "emailAddress",
-      message: "Hello $(firstName) what's your email address?",
-      type: "input",
-    },
-    {
-      name: "experienceDeveloper",
-      message: "Are you experienced developer?",
-      type: "list",
-      choices: ["yes", "no"],
-    },
-    {
-      name: "javascriptExperience",
-      message: "How many years of experience you have with JavaScript?",
-      type: "list",
-      choices: ["0-1", "1-3", "3-5", "5-10", "10+"],
-    },
-    {
-      name: "javascriptLibrary",
-      message: "What JavaScript library do you know?",
-      type: "checkbox",
-      choices: ["React.js", "Vue", "Angular", "Node.js", "jQuery", "D3.js"],
-    },
-    {
-      name: "desiredSalary",
-      message: "What is your desired salary?",
-      type: "input",
-    },
-  ])
+  .prompt(questions)
   .then((answers) => {
     console.log(JSON.stringify(answers, null, 2));
   })
